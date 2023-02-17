14024167f59517bb9de827598ae01c797e87e600
ogirians <ogi.rians3@gmail.com>
2023-02-17 22:04:26 +0700

    tambah card2

---
+++ pages/jkn.vue
@@ -0,0 +1,14 @@
+<template>
+    <div transition="slide-x-transition">
+        <h1>ini halaman Pendaftaran JKN</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    transition: 'slide-bottom'
+}
+</script>
+
+
+
---
+++ pages/umum.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card
+    color="#385F73"
+    dark
+    >
+        <v-card-title>
+            pendaftaran Umum
+        </v-card-title>
+        <v-card-subtitle>silahkan isi field berikut</v-card-subtitle>
+        <v-card-text>nama</v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default{
+
+}
+</script>
